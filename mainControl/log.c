2e222c79e81e437bf9c8055e73382c58f3351a66
jacklangille <jacklangille8@gmail.com>
2023-11-12 21:09:10 -0400

    Updated comments

--- mainControl/log.c
+++ mainControl/log.c
@@ -3,10 +3,10 @@
 #include <stdlib.h>
 #include <time.h>
 
-static FILE *log_file = NULL; // Declare a static file pointer. Initialize it to NULL.
+static FILE *log_file = NULL; 
 
 void init_log() {
-    log_file = fopen("uart_communication.log", "a"); // Open the log file in append mode
+    log_file = fopen("uart_communication.log", "a"); 
     if (log_file == NULL) {
         perror("Failed to open log file");
         exit(EXIT_FAILURE);
@@ -14,16 +14,12 @@ void init_log() {
 }
 
 void log_message(const char *message, const char *log_level) {
-    /*
-    Helper function for logging messages. Take message to log and level of logging.
-    */
-    if (log_file != NULL) {
-        // Get the current time for the log timestamp
-        time_t now = time(NULL);
+    if (log_file != NULL) { // Ensure file exists
+        time_t now = time(NULL); // Get current time 
         char *time_str = ctime(&now); // Convert to string
         time_str[24] = '\0';  // Remove new line character 
 
-        fprintf(log_file, "[%s] %s: %s\n", time_str, log_level, message);
+        fprintf(log_file, "[%s] %s: %s\n", time_str, log_level, message); // Format message
         fflush(log_file); // Flush buffer
     }
 }
