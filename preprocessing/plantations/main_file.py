e603d6643b710ea2f98abc61a2d0d07b88fdb3b7
Aniket Mukherjee <an469681@dal.ca>
2024-01-30 11:32:25 -0400

    Min max scaling

--- preprocessing/plantations/main_file.py
+++ preprocessing/plantations/main_file.py
@@ -1,6 +1,8 @@
 import rasterio
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 
-tiff_path = 'C:/DSS/DSS FOLDER 1 11TH NOV\DSS-MANTIS-OBC-AI/Images/tree-phen/trees_feb2020-Dec2021_10m/2020-02-21.tiff'
+tiff_path = 'C:/DSS/DSS FOLDER 1 11TH NOV/DSS-MANTIS-OBC-AI/Images/tree-phen/trees_feb2020-Dec2021_10m/2020-02-21.tiff'
 
 
 with rasterio.open(tiff_path) as src:
@@ -11,3 +13,11 @@ with rasterio.open(tiff_path) as src:
 
 print(f"Raster Shape: {data.shape}")
 print(f"Raster Metadata: {metadata}")
+
+# Min max scaling
+min_max_scaler = MinMaxScaler()
+data_minmax = min_max_scaler.fit_transform(data.reshape(-1, 1)).reshape(data.shape)
+
+print(f"Raster Shape: {data.shape}")
+print(f"Raster Metadata: {metadata}")
+
--- preprocessing/plantations/venv/Scripts/activate.bat
+++ preprocessing/plantations/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\DSS\DSS FOLDER 1 11TH NOV\DSS-MANTIS-OBC-AI\venv
+set VIRTUAL_ENV=C:\DSS\DSS FOLDER 1 11TH NOV\DSS-MANTIS-OBC-AI\preprocessing\plantations\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
