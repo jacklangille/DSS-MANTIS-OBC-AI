b75ed34dd4d409f6cf8a5b7b929e8485c8671406
Aniket Mukherjee <an469681@dal.ca>
2024-03-08 09:58:31 -0400

    Detector response calibration all funcns created

--- preprocessing/Image_Process_Model/detector_response.py
+++ preprocessing/Image_Process_Model/detector_response.py
@@ -1,33 +1,50 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from load_and_convert import image_tensors
+import cv2
 
 def radiometricCorrection(images):
     corrected_images = []
 
     for image in images:
+
         destriped_image = destripeImage(image)
+        interpolated_image = interpolateMissingScanLine(destriped_image)
+        after_vignetting_removal=correctVignetting(interpolated_image)
+        denoised_image = removeRandomNoise(after_vignetting_removal)
 
         # Normalization to each band
-        normalized_image = (destriped_image - np.min(destriped_image)) / (np.max(destriped_image) - np.min(destriped_image))
-
+        normalized_image = (denoised_image - np.min(denoised_image)) / (np.max(denoised_image) - np.min(denoised_image))
         corrected_images.append(normalized_image)
 
     return corrected_images
 
-def destripeImage(image):
-    # Interpolate missing scan line and destripe 
+
+def interpolateMissingScanLine(image):
+    # Interpolate missing scan line (simple linear interpolation)
     for y in range(1, image.shape[0] - 1):
         missing_scan_line_indices = np.isnan(image[y, :])
         if np.any(missing_scan_line_indices):
             for x in range(image.shape[1]):
                 if missing_scan_line_indices[x]:
-                  
+                    # Linear interpolation for missing scan line
+                    image[y, x] = (image[y - 1, x] + image[y + 1, x]) / 2
+
+    return image
+
+def destripeImage(image):
+    #Destripe the image (using the provided formula)
+    for y in range(1, image.shape[0] - 1):
+        missing_scan_line_indices = np.isnan(image[y, :])
+        if np.any(missing_scan_line_indices):
+            for x in range(image.shape[1]):
+                if missing_scan_line_indices[x]:
+              
                     image[y, x] = destripingFormula(image, y, x)
 
     return image
 
-# De-striping formula
+#De-striping formula
 def destripingFormula(image, y, x):
     od = 255
     oi = 255
@@ -36,7 +53,16 @@ def destripingFormula(image, y, x):
 
     return (od / oi) * image[y - 1, x] + md - (od / oi) * mi
 
-# Example Usage:
+def correctVignetting(image):
+    clahe = cv2.createCLAHE()
+    corrected_image = clahe.apply(image)
+    return corrected_image
+
+# Replacing each pixel value with the median value of its neighboring pixels within a specified kernel size
+def removeRandomNoise(image):
+    denoised_image = cv2.medianBlur(image.astype(np.uint8), 5)  # The kernel size here would need to be adjusted
+    return denoised_image
+
 radiometrically_corrected_images = radiometricCorrection(image_tensors)
 
 # Section 4: Displaying Radiometrically Corrected Images (Optional)
