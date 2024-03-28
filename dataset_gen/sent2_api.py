acdc2ee7ada2d5e3898eaf570b705649b34b74b1
Jack Langille <lwj1396@proton.me>
2024-03-28 17:50:41 -0300

    Update to request script

--- dataset_gen/sent2_api.py
+++ dataset_gen/sent2_api.py
@@ -1,15 +1,24 @@
 import pandas as pd
 import matplotlib.image as img
-from sentinelhub import SentinelHubRequest, SHConfig, DataCollection, MimeType, CRS, BBox, bbox_to_dimensions, Geometry
+from sentinelhub import (
+    SentinelHubRequest,
+    SHConfig,
+    DataCollection,
+    MimeType,
+    CRS,
+    BBox,
+    bbox_to_dimensions,
+    Geometry,
+)
 import evalscripts as eva
 
-CONFIG = SHConfig() 
+CONFIG = SHConfig()
 OUTPUT_DIR = "../dataset_gen/results/"
-RES = 20 # Resolution in meters
-TIME_INTERVAL = ("2023-10-30", "2023-11-6")
+RES = 20  # Resolution in meters
+TIME_INTERVAL = ("2023-10-30", "2023-11-6")  # Date range for images
 
 # Original coordinates
-coordinates =[4.1, 62.04, 4.887440854041823, 62.39932626325639]
+coordinates = [4.1, 62.04, 4.887440854041823, 62.39932626325639]
 # Create a bbox from the original coordinates
 bbox = BBox(bbox=coordinates, crs=CRS.WGS84)
 
@@ -20,19 +29,17 @@ print(f"Original dimensions: {dimensions}")
 
 # Adjusted request with new_bbox
 request = SentinelHubRequest(
-    evalscript=eva.algae,  
+    evalscript=eva.algae,
     input_data=[
         SentinelHubRequest.input_data(
             data_collection=DataCollection.SENTINEL2_L1C,
             time_interval=TIME_INTERVAL,
         )
     ],
-    responses=[
-        SentinelHubRequest.output_response('default', MimeType.JPG)
-    ],
+    responses=[SentinelHubRequest.output_response("default", MimeType.JPG)],
     bbox=bbox,  # Use the adjusted BBox object
     size=dimensions,  # Use the new dimensions
-    config=CONFIG
+    config=CONFIG,
 )
 
 img_response = request.get_data()
